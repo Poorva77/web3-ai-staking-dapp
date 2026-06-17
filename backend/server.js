d3ce756615d57f6c159eda2ae05a5dfbe85981a8
Poorva Jaiswal <poorvajaiswal2708@gmail.com>
2026-06-17 18:18:00 +0530

    render check

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,16 @@ app.use(express.json());
 
 app.use("/api/stake", stakeRoutes);
 
-app.listen(5000, () => {
+/* app.listen(5000, () => {
     console.log("Backend running on port 5000");
+}); */
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+
+    console.log(
+        `Backend running on port ${PORT}`
+    );
+
 });
